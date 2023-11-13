b3a38ba2be11c6974c8f5fe669b83a4308c9aac4
Marco <marcoregina92@gmail.com>
2023-11-13 10:33:41 +0100

    completa Milestone 0 e Milestone 1

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+const staff = [
+    {
+        nome: "Wayne Bernett",
+        posizione: "Founder & CEO",
+        img: "wayne-bernett-founder-ceo.jpg"
+    },
+
+    {
+        nome: "Angela Caroll",
+        posizione: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        nome: "Walter Gordon",
+        posizione: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+
+    {
+        nome: "angelaLopez",
+        posizione: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        nome: "Scott Estrada",
+        posizione: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+
+    {
+        nome: "Barbara Ramos",
+        posizione: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    },
+];
+
+
+
+for (let i = 0; i < staff.length; i++) {
+    const person = staff[i];
+    console.log(`Nome: ${person.nome}, Posizione: ${person.posizione}, Immagine: ${person.img}` );
+
+}
